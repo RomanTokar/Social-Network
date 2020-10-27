3edc1860da2838d3d1004b9b2383ebdbdd734273
RomanTokar <49822676+RomanTokar@users.noreply.github.com>
2020-10-27 18:24:38 +0200

    Add Alert into UsersList.js

--- src/components/Main/UsersPage/UsersList.js
+++ src/components/Main/UsersPage/UsersList.js
@@ -1,6 +1,7 @@
 import React, {memo} from 'react';
 import Grid from '@material-ui/core/Grid';
 import UserItem from './UserItem';
+import Alert from '@material-ui/lab/Alert';
 
 const UsersList = ({users, isFetching}) => {
   const defaultUser = {
@@ -18,7 +19,11 @@ const UsersList = ({users, isFetching}) => {
             ? users.map((user) => <UserItem key={user.id} isFetching={isFetching}>{user}</UserItem>)
             :
             <Grid container justify={'center'}>
-              <Grid item>Nothing wasn't found.</Grid>
+              <Grid item>
+                <Alert severity="info">
+                  Nothing wasn't found
+                </Alert>
+              </Grid>
             </Grid>
         )
         : new Array(45)
